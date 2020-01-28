fixed bug in bineary search, matched hopf analysis to numerical simulations part 1

Simulation_Analysis_Toolset/__init__.py
@@ -12,6 +12,7 @@ from scipy.integrate import solve_ivp
 from scipy.signal import argrelextrema
 from abc import ABC, abstractmethod 
 import cmath
+from builtins import int
     
 
 class DynamicalSystemSim(ABC):
@@ -46,7 +47,9 @@ class DynamicalSystemSim(ABC):
             self.deriv,   # Derivative function
             (self.t0, self.T),       # Total time interval
             self.X0,  # Initial State
-            t_eval = np.arange(self.t0, self.T, self.dt)  # Returned evaluation time points
+            t_eval = np.arange(self.t0, self.T, self.dt),  # Returned evaluation time points
+            method='LSODA',  #Radau solver for stiff systems
+            dense_output=True
             )
         
         sim_data = {}
@@ -102,7 +105,7 @@ class OscillatorySimAnalyzer(ABC):
             
             pert_sim = sim.copy_sim()
             pert_sim.X0 = limit_cycle['X'][:, idx] + u[:, idx]
-            pert_sims['pert_idx=%i'%idx] = pert_sim.run_sim()
+            pert_sims['%i'%idx] = pert_sim.run_sim()
         
         print('Perturbed Simulations Complete...')
         return pert_sims
@@ -131,7 +134,7 @@ class OscillatorySimAnalyzer(ABC):
             for t in limit_cycle['t'] - t0:
                 phases.append(t/T)
                 
-        return phases
+        return np.asarray(phases)
     
     
     def get_traj_dist_from_lc(self, limit_cycle, traj):
@@ -145,9 +148,8 @@ class OscillatorySimAnalyzer(ABC):
              for i in np.arange(traj.shape[1])
              ])
              
+    {}  
     ## Abstract methods need implementation 
-
-
     def idx_of_lc_convergence(self, limit_cycle, trajectory, delta):
         '''
         Given a limit cycle, a distance delta, and trajectory (Dim, N array).
@@ -156,48 +158,55 @@ class OscillatorySimAnalyzer(ABC):
         the trajectory is *always* within delta of the limit cycle after the
         returned index.
         '''
-        pass
     
         def converged(X, limit_cycle, delta):
             ''' Check if a point X is within delta of limit_cycle '''
             
-            if self.dist_to_limit_cycle(limit_cycle, X) <= delta:
+            if np.less_equal(self.dist_to_limit_cycle(limit_cycle, X), delta):
+
                 return True
             
             else:
                 return False
     
     
-        def bin_search_conv_idx(idxs):
+        def bin_search_conv_idx(trajectory, limit_cycle, delta):
             '''
             Recursively implement binary search to find smallest index of convergence
             in given trajectory
             '''
             
-            if len(idxs) == 0:
-                return -1
-            else:
-                midpoint = np.ceil(len(idxs)/2)
-                search_idx = idxs[midpoint]
-                curr_conv = converged(limit_cycle, trajectory[search_idx], delta)
-                prev_conv = converged(limit_cycle, trajectory[search_idx - 1], delta)
+            if converged(trajectory[:,0], limit_cycle, delta):
+                return 0
+            
+            
+            left = 0
+            right = len(trajectory[0,:])-1
+            
+            
+            while right - left > 0:
+                if left < 0 or right < 0 :
+                    raise Exception("negative index during binary search: left = %i, right = %i"%(left, right))
                 
-            if curr_conv: # if current converged
+                midpoint = left +  int(
+                    np.ceil((right - left) / 2)
+                    )
+                
+                if converged(trajectory[:,midpoint], limit_cycle, delta):
+                    if not converged(trajectory[:,midpoint-1], limit_cycle, delta):
+                        return midpoint
                     
-                if not prev_conv: # if previous not converged stop
-                    return search_idx
-                
-                else: # otherwise check left half
-                    return bin_search_conv_idx(idxs[0:midpoint])
-                        
-            
-            else: #check right half
-                return bin_search_conv_idx(idxs[midpoint:])
-            
-            
-        
-        # find first point of convergence
-        conv_idx = bin_search_conv_idx(np.arange(trajectory.shape[1]))
+                    else:
+                        right = midpoint  
+                    
+                else:
+                    left = midpoint
+                    
+                     
+            return -1
+         
+
+        conv_idx = bin_search_conv_idx(trajectory, limit_cycle, delta)
 
 
         if conv_idx is  -1: #if binsearch fails

andronov-hopf_plots_1_25_2020.py
@@ -1,6 +1,9 @@
 
 # exploration & plotting of analytic results form andronov-hopf oscillator
  
+### TODO:
+## Simulate & compute voltage approximation error vs phi/ epsilon 
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +11,14 @@ import matplotlib.cm as cm
 import Simulation_Analysis_Toolset as sat
 
 class AndronovHopfSim(sat.DynamicalSystemSim):    
+    ''' 
+    AndronovHopfSim is meant to simulate the normal form limit cycle oscillator,
+    Andronov-Hopf oscillator, which undergoes a Hopf bifurcation.
+    Starting coordinates should always be given in cartesian coordinates,
+    although internally it computes derivative via polar coordinates 
+    '''
     def __init__(self, X0, T, dt, t0 = 0, w = 1):
+        
         super().__init__(X0, T, dt, t0)    
         self.w = w
         
@@ -20,6 +30,7 @@ class AndronovHopfSim(sat.DynamicalSystemSim):
         dphi/dt = 1 
         '''
         return np.asarray([X[0]-X[0]**3, self.w])
+
     
     def copy_sim(self):
         return AndronovHopfSim(
@@ -27,27 +38,29 @@ class AndronovHopfSim(sat.DynamicalSystemSim):
             T = self.T,
             t0 = self.t0,
             dt = self.dt,
-            w = self.w
+            w = self.w,
             )
+
         
     def run_sim(self):
+        #convert cart to polar
+        r = self.X0[0]
+        phi =  self.X0[1]
+        x,y = cart_to_polar(r, phi)
+        self.X0 = np.asarray([x,y])
+    
+        #run sim
         data =  super().run_sim()
         data["w"] = self.w
+        data['r'] = data['X'][0,:]
+        data['phi'] = data['X'][1,:]
+        self.X0 = np.asarray([r, phi])
         
-        #convert from polar to cartesian coords
-        rs = data['X'][0,:]
-        phis = data['X'][1,:]
-        
-        xs = rs * np.cos(phis)
-        ys = rs * np.sin(phis)
-        
+        # convert polar to cart
+        xs, ys = polar_to_cart(data['X'][0,:], data['X'][1,:])
         data['X'][0,:] = xs
         data['X'][1,:] = ys
-        data['r'] = rs
-        data['phi'] = phis
-         
-        
-        
+  
         return data
 
     
@@ -59,14 +72,14 @@ class AndronovHopfAnalyzer(sat.PlanarLimitCycleAnalyzer):
     def lc_eigen_decomp(self, sim_data, limit_cycle):
         pass
 
+
         
 def ttc(eps, delta):
     ''' time to convergence as function of perturbation along radial axis'''
-    num = -(1+2*delta+delta**2)
-    denom = 2*delta+delta**2
-    coeff = 1/(1+eps)**2 - 1
-    
-    return 1/2 * np.log(num / denom * coeff)
+    num = -(1+2*delta+np.square(delta))
+    denom = 2*delta+np.square(delta)
+    coeff = 1/np.square(1+eps) - 1 
+    return 1/2 * np.log(np.divide(num * coeff , denom ))
 
 
 def r_t(eps, t):
@@ -104,19 +117,39 @@ def rmse(eps, phi_0, w):
         )
 
 
+def polar_to_cart(rs, thetas):
+    ''' convert polar coords to cartesian coords'''
+    
+    xs = rs * np.cos(thetas)
+    ys = rs * np.sin(thetas)
+    return (xs, ys)
+
+
+def cart_to_polar(xs, ys):
+    ''' convert cartesian coords to polar '''
+    rs = np.sqrt(np.square(xs) + np.square(ys))
+    thetas = np.arctan2(ys, xs)
+    return (rs, thetas)
+
+    
 {}
-# # Figure Configuration
-plt.rcParams['figure.figsize'] = [6, 4.5]
+## Figure Configuration
+plt.rcParams['figure.figsize'] = [8, 4.5]
 plt.rcParams['figure.dpi'] = 200
     
     
+## Simulation Paramters
 epsilon = .5  # perturbation strength
 r0   = 1 + epsilon  #radial perturbation by epsilon
 phi0 = 0
-T = 30
-dt = .001
+period = 1
+x, y = polar_to_cart(r0, phi0)
+T = 25
+dt = .0001
+delta = .001
 
-sim = AndronovHopfSim(X0 = np.asarray([r0, phi0]), T = T, dt = dt)
+## Run Simulation
+sim = AndronovHopfSim(X0 = np.asarray([x, y]), T = T, dt = dt, w = 2*np.pi / period)
 aha = AndronovHopfAnalyzer()
 data = sim.run_sim()
     
@@ -140,9 +173,10 @@ plot_limit_cycle           = 0   # Assuming at least 2 full periods of oscillati
 
 plot_traj_perturbations    = 0   # Numerically simulate a given perturbation along given points of a limit cycle
 
-plot_pert_along_evecs      = 0   # Perturb the limit cycle along an eigenvector and plot the convergence results
+plot_conv_analysis         = 0   # Simulate given perturbation for chosen indices & compute their distance to limit cycle vs phase/phi
 
-plot_convergence_analysis  = 0   # Simulate given perturbation for chosen indices & compute their distance to limit cycle vs phase
+
+
 
 
 
@@ -287,6 +321,103 @@ if (plot_limit_cycle):
     plt.plot(np.cos(phis),np.sin(phis),'--',label='Analytic')
     plt.legend()
     plt.title("Limit Cycle Phase Portrait")
+
+if (plot_traj_perturbations):
+    print('Plotting perturbation trajectories...')
+
+    if not plot_limit_cycle:
+        limit_cycle = aha.get_limit_cycle(data)
+    
+    
+    # given indices, perturb them along an eigenvector and plot the convergence results
+    idxs = np.linspace(0, len(limit_cycle['t'])-1,num = 100, dtype = int )
+    # add an (x,y) component for each perturbation
+    # for each point, compute (x,y), scale by 1 + epsilon that u
+    us = (1 + epsilon) * limit_cycle['X']
+    pert_sims = aha.perturb_limit_cycle(sim, limit_cycle, us, idxs)
+    
+    plt.figure("perturbation_trajectories")
+    for i in idxs:
+        pert_xs = pert_sims['%i'%i]['X'][0,:]
+        pert_ys = pert_sims['%i'%i]['X'][1,:]
+        plt.plot(pert_xs, pert_ys, label='sim %i'%i)
+    
+    plt.xlabel('X (Voltage)')
+    plt.ylabel('Y')
+    plt.title('Perturbations Along Radial Axis')
+       
+if (plot_conv_analysis):
+    print("Plotting convergence analysis (sweeping phi)...")
+    
+    if not plot_limit_cycle:
+        limit_cycle = aha.get_limit_cycle(data)
+    # perturb along radial axis, compute ttc, sweep phase and phi
+    
+    # Sweep along phi and perturb radial axis
+    # given indices, perturb them along an eigenvector and plot the convergence results
+    idxs = np.linspace(0, len(limit_cycle['t'])-1,num = 10, dtype = int )
+    # add an (x,y) component for each perturbation
+    # for each point, compute (x,y), scale by 1 + epsilon that u
+    us = (1 + epsilon) * limit_cycle['X']
+    pert_sims = aha.perturb_limit_cycle(sim, limit_cycle, us, idxs)
+    
+    phases = aha.lc_times_to_phases(limit_cycle, False)
+    ttcs = []
+    
+    print('Computing Convergences...')
+    for i, idx in enumerate(idxs):
+        print('%i/%i'%(i+1, len(idxs)))
+        # compute ttc of trajectory
+        conv_idx = aha.idx_of_lc_convergence(limit_cycle, pert_sims[str(idx)]['X'], delta)
+        ttcs.append(pert_sims[str(idx)]['t'][conv_idx])
+        
+        
+        
+    plt.figure("ttc_vs_phi")
+    plt.plot(phases[idxs], ttcs/aha.lc_period(limit_cycle), label='Simulated')
+    plt.axhline(ttc(epsilon, delta)/aha.lc_period(limit_cycle),label='Analytic')
+    
+    plt.xlabel("$\phi_{0}$")
+    plt.ylabel("Time to convergence")
+    plt.title("Time to convergenc e versus (radial) perturbation phase. $\epsilon = %.2f$, $\delta$ = $ \epsilon/1000$"%epsilon)
+    plt.legend()
+    
+    
+    
+
+    print("Plotting convergence analysis (sweeping epsilon)...")
+    
+    if not plot_limit_cycle:
+        limit_cycle = aha.get_limit_cycle(data)
+    # perturb along radial axis, compute ttc, sweep phase and phi
+    
+    epsilons = np.linspace(0.1, 10, num = 25)
+    deltas = epsilons / 1000
+    #deltas = np.ones(len(epsilons)) * delta
+    ttcs_numeric = []
+    ttcs_exact = []
+    
+    # for epsilons, simulate trajectory starting at limit_cycle[0] + epsilon (in xdir)
+    for i, eps in enumerate(epsilons):
+        print("%i/%i"%(i+1, len(epsilons)))
+        pert_sim = sim.copy_sim()
+        pert_sim.X0 = np.asarray([1 + eps, 0]) # perturb radially at phi = 0
+        pert_data = pert_sim.run_sim()
+        ttcs_numeric.append(pert_data['t'][aha.idx_of_lc_convergence(limit_cycle, pert_data['X'], deltas[i])])
+        ttcs_exact.append(ttc(eps, deltas[i]))
+    
+    #plot and compare to analytic ttc
+    plt.figure("ttcs_vs_epsilon")
+    plt.plot(epsilons, ttcs_numeric / aha.lc_period(limit_cycle), label= 'Simulated')
+    plt.plot(epsilons, np.asarray(ttcs_exact) / period ,label= 'Analytic')
+    plt.xlabel("$\epsilon$")
+    plt.ylabel("Time to convergence (fractions of period)")
+    plt.title("Time to convergence versus (radial) perturbation strength. $\phi_0 = 0$, $\delta$ = $ \epsilon/1000$")
+    plt.legend()
+    
+    #plt.savefig('ttc_vs_perturbation_strength_delta_frac.png',bbox_inches = 'tight')
+        
+    
 
 print("Simulation Complete.")
 plt.show()

van_der_pol_1_26_2020.py
@@ -8,7 +8,6 @@ import cmath
 
 
 ## TODO:
-# finish importing plotting from old vanderpol code (1_15_2020)
 # latent_phase computation & implementation
 
 class VanderPolSim(sat.DynamicalSystemSim):
@@ -139,9 +138,7 @@ plot_eigen_decomp          = 0   # Compute the eigenvalues/eigenvectors along th
 
 plot_perturbation_analysis = 0   # Perturb along an eigenvector & compute its linearized growth for each point on the limit cycle
 
-plot_traj_perturbations    = 0   # Numerically simulate a given perturbation along given points of a limit cycle
-
-plot_pert_along_evecs      = 0   # Perturb the limit cycle along an eigenvector and plot the convergence results
+plot_traj_perturbations    = 1   # Numerically simulate a given perturbation along given points of a limit cycle
 
 plot_convergence_analysis  = 0   # Simulate given perturbation for chosen indices & compute their distance to limit cycle vs phase
 
@@ -283,61 +280,29 @@ if (plot_perturbation_analysis):
     
 if (plot_traj_perturbations):
     print('Plotting perturbation trajectories...')
+
     if not plot_limit_cycle:
-        lc_x, lc_y, _ = get_limit_cycle(data)
-    
-    if not (plot_eigen_decomp):
-        ed = eigen_decomp(data)
-
-    #find index of point on limit cycle with maximum eigenvalue(neg)
-
-    max_idx = np.argmax(ed["l_neg"])
-    min_idx = np.argmin(ed["l_neg"])
-    idxs = np.linspace(0,len(lc_x)-1,num = 100,dtype = int)
-    #idxs = [max_idx, min_idx]
-    #idxs = [-4] 
-    pts = perturb_limit_cycle(data, lc_x, lc_y,  u, indices = idxs)
-    mins = np.zeros((pts.shape[1],pts.shape[2]))
-    plt.figure()    
-    for i in np.arange(len(idxs)):
-        print('%i/%i'%(i+1,len(idxs)))
-        plt.plot(pts[0,:,i],pts[1,:,i])
-
-    plt.plot(lc_x,lc_y,c='red')           
-
-if plot_pert_along_evecs:
-    if not plot_limit_cycle:
-        lc_x, lc_y, lc_t = get_limit_cycle(data)
+        limit_cycle = vpa.get_limit_cycle(data)
     
     if not plot_eigen_decomp:
-        ed = eigen_decomp(data)
+        ed = vpa.lc_eigen_decomp(data, limit_cycle)
     
     # given indices, perturb them along an eigenvector and plot the convergence results
-    idxs = np.linspace(0, len(lc_x)-1,num = 800, dtype = int )
-    fig, ax1 = plt.subplots()
-    ttcs = []
-    for j,i in enumerate(idxs):
-        print("%i/%i..."%(j+1,len(idxs)))
-        evec = epsilon * ed["evec_neg"][:,i]
-        pert = (lc_x[i] + evec[0], lc_y[i] + evec[1])
-        # compute a trajectory starting at pert
-        pert_data = VanderPolSim(mu = mu, x0 = pert[0], y0 = pert[1], T = T, dt = dt).run_sim()    
-        pert_traj_x = pert_data.y[0,:]
-        pert_traj_y = pert_data.y[1,:]
-        pert_traj_ts = pert_data.t
-        
-        # compute time to convergence
-        ttcs.append(time_to_convergence(lc_x, lc_y, pert_traj_x, pert_traj_y, pert_traj_ts, delta))
-        
-        
-    ax1.scatter(lc_t[idxs] / lc_period(lc_t),ttcs, label = "time to convergence")
+    idxs = np.linspace(0, len(limit_cycle['t'])-1,num = 100, dtype = int )
+    us = limit_cycle['X'] + epsilon * ed['evec_neg']
+    pert_sims = vpa.perturb_limit_cycle(sim, limit_cycle, us, idxs)
     
-    ax2 = ax1.twinx()
+    plt.figure("perturbation_trajectories")
+    for i in idxs:
+        pert_xs = pert_sims['%i'%i]['X'][0,:]
+        pert_ys = pert_sims['%i'%i]['X'][1,:]
+        plt.plot(pert_xs, pert_ys, label='sim %i'%i)
+    
+    plt.xlabel('X (Voltage)')
+    plt.ylabel('Y')
+    plt.title('Perturbations Along Radial Axis$')
+       
 
-    ax2.plot(lc_t[idxs]/ lc_period(lc_t), ed["l_neg"][idxs],'--',label='$\lambda_{-}$')    
-    ax2.plot(lc_t[idxs]/ lc_period(lc_t), ed["l_pos"][idxs],'--',label='$\lambda_{+}$')
-    ax2.axhline(y=0)
-    plt.legend()
 
 if (plot_convergence_analysis):
     print("Plotting convergence analysis...")