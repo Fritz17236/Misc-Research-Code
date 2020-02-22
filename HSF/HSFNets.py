3342426b7d56bb7f7b9e8300be9f4fa6b46a2f20
Fritz17236 <fritz17236@hotmail.com>
2020-02-21 18:29:11 -0800

    implemented event-based simulation changes, next need to work out spike mechanism in classic deneve

--- HSF/HSFNets.py
+++ HSF/HSFNets.py
@@ -12,7 +12,9 @@ import matplotlib.pyplot as plt
 from abc import abstractmethod
 from scipy.integrate._ivp.ivp import solve_ivp
 
-
+'''
+Class Definitions
+'''
 
 class SpikingNeuralNet(sat.DynamicalSystemSim):
     ''' 
@@ -32,13 +34,72 @@ class SpikingNeuralNet(sat.DynamicalSystemSim):
         self.lam = lam #leak term
         self.D = D # Decoder matrix
         
-        self.O = [np.asarray([]) for i in np.arange(self.N)] #spike raster is list of empty lists
+        self.O = {}
+        for i in np.arange(self.N):
+            self.O[str(i)] = np.empty((1,))
+        
         super().__init__(lds.X0, T, dt, t0)
     
+    
+    def run_sim(self, event=None):
+        ts = None
+        t_events = None
+        X = None
+        X0 = self.X0
+        t0 = self.t0
+        while True:
+            data = solve_ivp(
+                self.deriv, # Derivative function
+                (t0, self.T), # Total time interval
+                X0, # Initial State
+                t_eval=np.arange(t0, self.T, self.dt),  
+                method='LSODA', 
+                dense_output=True, 
+                events=event
+                )
+            if X is None:
+                X = data.y
+                ts = data.t
+                t_events = data.t_events
+            else:
+                X = np.append(X, data.y, axis = 1)
+                ts = np.append(ts, data.t )
+                t_events = np.append(t_events, data.t_events,axis=1)
+            
+            
+            if event and data.status == 1: # if spike threshold crossed, spike
+                t0 = data.t[-1] + dt
+                X0 = data.y[:, -1].copy()
+                _, idxs = event(data.t[-1], X0, ret_idx=True)
+                self.spike(idxs)
+            else:
+                break
+    
+        sim_data = {}
+        for param in self.__dict__.keys():
+            sim_data[str(param)] = self.__dict__[param]
+        
+        sim_data['X'] = X
+        sim_data['t'] = ts
+        sim_data['t_events'] = np.asarray(t_events)
+        return sim_data
     
     @abstractmethod
     def deriv(self, t, X):
         pass
+    
+    @abstractmethod
+    def neuron_spikes(self, t, y, ret_idxs = False):
+        '''
+        Based on State & Time decide whether neuron(s) spikes. 
+        Used by root finding algorithm to determine when neuron voltage 
+        v- thresh > 0, i.e when a neuron crosses threshold voltage.
+        ret_idxs is set to true means that the index of any spiking neuron will
+        be returned.
+        '''
+    neuron_spikes.terminal = True # Set both attributes for use in event-based spike detection
+    neuron_spikes.direction = 1
+     
     @abstractmethod
     def spike(self, indices):
         ''' 
@@ -46,11 +107,18 @@ class SpikingNeuralNet(sat.DynamicalSystemSim):
         each neuron at indices[i] according to the spike rule
         '''
         assert( len(indices) <= self.N)
-    # r(t) leaky firing rates computed from o(t)  
+
+
+class DeneveNet(SpikingNeuralNet):
+    ''' Spiking Net According to Classic Deneve Paper '''
     
-    # run -- how? 
-
-
+    def deriv(self, t, X):
+        pass
+    
+'''
+Helper functions
+'''
+    
 def gen_decoder(d, N, mode='random'):
         '''
         Generate a d x N decoder mapping neural activities to 
@@ -58,6 +126,10 @@ def gen_decoder(d, N, mode='random'):
         draws decoders from 0 mean gaussian with std=1
         '''
         return np.random.normal(loc=0, scale=1, size=(d, N) )
+
+
+
+
 
 A =  np.zeros((2,2))
 A[0,1] = 1
@@ -75,54 +147,24 @@ D = gen_decoder(len(x0), N)
 
 #net = SpikingNeuralNet(T, dt, N, D, lds, lam)
 
-def run_sim(self, event=None):
-    ts = np.empty((1,))
-    t_events = np.empty((1,))
-    X = np.empty_like(self.X0)
-    X0 = self.X0
-    t0 = self.t0
-    while True:
-        data = solve_ivp(
-            self.deriv, # Derivative function
-            (t0, self.T), # Total time interval
-            X0, # Initial State
-            t_eval=np.arange(t0, self.T, self.dt),  
-            method='LSODA', 
-            dense_output=True, 
-            events=event
-            )
-        X = np.append(X, data.y, axis = 1 )
-        ts = np.append(ts, data.t )
-        #t_events = np.append(data.t_events,axis=1)
-        
-        
-        if event and data.status == 1: # Event was hit
-            # New start time for integration
-            t0 = data.t[-1]
-            # Reset initial state
-            X0 = data.y[:, -1].copy()
-            X0[0] += 1
-        else:
-            break
+
         
     
 
-    sim_data = {}
-#     for param in self.__dict__.keys():
-#         sim_data[str(param)] = self.__dict__[param]
-    
-    sim_data['X'] = X
-    sim_data['t'] = ts
-    sim_data['t_events'] = np.asarray(t_events)
-    return sim_data
      
 
 
-def event(t, y):
-    ''' blah'''
+def neuron_spikes(t, y, ret_idx = False):
+    '''
+    Returns the neuron closest to passing above  
+    '''
     
     thresh = .5
-    return y[0] - thresh
+    
+    if ret_idx:
+        y[0] - thresh, 0
+    else:
+        return y[0] - thresh
 
 # event based state updating
 # if event occurs (x > thresh), record event, and move x += delta
@@ -133,7 +175,8 @@ data = lds.run_sim(lds, event)
 
 
 print(data.keys())
-print(data['X'][0].shape)
-#plt.plot(data['t'],data['X'][0,:])
-#plt.axvline(x = data['t_events'][0][0])
+
+
+plt.plot(data['t'],data['X'][0,:])
+plt.axvline(x = data['t_events'][0][0])
 plt.show()
